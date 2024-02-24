f8df4b2bf77ea97e09d2ec31221696890b46ca1c
ACER <ACER@LAPTOP-P832L3NQ>
2024-02-24 21:23:04 +0700

    Add project files.

---
+++ Library_Management/Components/Src/Dbconnect.cs
@@ -0,0 +1,14 @@
+﻿
+//using Microsoft.EntityFrameworkCore;
+
+
+////connect to sql server
+//public class Dbconnect : DbContext
+//{
+//    public DbSet<Book> Books { get; set; }
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//    {
+//        optionsBuilder.UseSqlServer("Server=your_server_ip;Database=your_database_name;User Id=your_username;Password=your_password;");
+//    }
+//}
